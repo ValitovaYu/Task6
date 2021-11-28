0feaf016f5dabe0f8b3e1649937fd14d1f0eb8a9
Юлия Валитова <valitovajulian@gmail.com>
2021-11-28 17:40:39 +0300

    Добавьте файлы проекта.

---
+++ 2_Task6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Task6
+{
+    class Program
+    {
+        /* Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+         Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом 
+        без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»)*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение, состоящее из слов, разделенных пробелом, без знаков препинания");
+            string s = Console.ReadLine();
+            s = s.ToLower();
+            s = s.Replace(" ", "");
+
+
+            char[] sArray = s.ToCharArray();
+            Array.Reverse(sArray);
+            string sFinal = new string(sArray);
+
+            if (s == sFinal)
+            {
+                Console.WriteLine("палиндром");
+            }
+            else
+            Console.WriteLine("НЕ палиндром");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        /* Ввести с клавиатуры предложение.Предложение представляет собой слова, разделенные пробелом. 
+         Знаки препинания не используются. Найти самое длинное слово в строке.*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение, состоящее из слов, разделенных пробелом, без знаков препинания");
+            string startString = Console.ReadLine();
+            string [] arrayString = startString.Split();                       
+            string maxWord= string.Empty;
+            for (int i = 0; i < arrayString.Length; i++)
+            {
+                if (arrayString[i].Length> maxWord.Length)
+                {                    
+                    maxWord = arrayString[i];
+                }
+
+            }
+                        
+            Console.WriteLine("Самое длинное слово: {0}", maxWord);
+            Console.ReadKey();
+
+        }
+    }
+}
